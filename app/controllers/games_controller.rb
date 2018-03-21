891cb96f856c9637d57c2fdc09059949c693e910
github username <github email address>
2018-03-20 20:13:29 -0400

    fixed relationships

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -46,6 +46,6 @@ class GamesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def game_params
-      params.require(:game).permit(:players)
+      params.fetch(:game){}
     end
 end
